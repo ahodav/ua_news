895266cbcea048ece6136e5d43c0c564fe4750ac
Orest Yaryha <o.yaryha@ticketstravelnetwork.com>
2024-03-24 17:12:53 +0200

    use memory

--- telegram.js
+++ telegram.js
@@ -2,24 +2,19 @@ import axios from "axios";
 import fs from "node:fs/promises";
 
 const sendToTelegram = ({ link_original: url, title }) => {
-  axios.post(
-    process.env.TELEGRAM,
-    {
-      chat_id: "@science_ua_news",
-      link_preview_options: {
-        prefer_large_media: true,
-      },
-      parse_mode: "html",
-      text: `<a href="${url}">${title}</a>`,
-    }
-  );
+  axios.post(process.env.TELEGRAM, {
+    chat_id: "@science_ua_news",
+    link_preview_options: {
+      prefer_large_media: true,
+    },
+    parse_mode: "html",
+    text: `<a href="${url}">${title}</a>`,
+  });
 };
 
-export default async (data) => {
-  const prevNewsIds = await fs.readFile("./newsIds").then((res) =>
-    JSON.parse(res.toString())
-  );
+let prevNewsIds = [];
 
+export default async (data) => {
   const postToSend = data.filter(({ id }) => !prevNewsIds.includes(id));
 
   postToSend.forEach((post) => {
@@ -28,7 +23,5 @@ export default async (data) => {
     }, Math.random() * 60000);
   });
 
-  const idsToSave = data.map(({ id }) => id);
-
-  fs.writeFile("./newsIds", JSON.stringify(idsToSave, null, 2));
+  prevNewsIds = data.map(({ id }) => id);
 };
